691e5c38675ae510753aec1f401758ebcc40e5c5
Artur Trifonov <wrgraff@gmail.com>
2021-09-06 20:22:51 +0300

    Возвращает реализацию со значением по умолчанию

--- src/view/smart.js
+++ src/view/smart.js
@@ -6,7 +6,7 @@ export default class Smart extends Abstract {
     this._data = {};
   }
 
-  updateData(update, justDataUpdating) {
+  updateData(update, justDataUpdating = false) {
     if (!update) {
       return;
     }
