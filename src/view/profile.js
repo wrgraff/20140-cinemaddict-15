3ac360a78f9172c55b36644111709dc4268eddf4
Artur Trifonov <wrgraff@gmail.com>
2021-09-22 20:47:48 +0300

    Убирает имя при нулевом рейтинге из профиля

--- src/view/profile.js
+++ src/view/profile.js
@@ -1,8 +1,10 @@
 import AbstractView from '@view/abstract.js';
 
+const createProfileRatingTemplate = (isLoading, rank) => `<p class="profile__rating">${isLoading ? 'Loading...' : rank}</p>`;
+
 const createProfileTemplate = (isLoading, rank) => (`
   <section class="header__profile profile">
-    <p class="profile__rating">${isLoading ? 'Loading...' : rank}</p>
+    ${ rank !== '' ? createProfileRatingTemplate(isLoading, rank) : '' }
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>
 `);
